4c7833f4f0bdaa61089dcd47c6d810ed06a119cc
mjgwakk <mjgwakk@gmail.com>
2025-10-21 20:17:03 +0900

    update model path

--- src/ngram_acc.sh
+++ src/ngram_acc.sh
@@ -3,8 +3,8 @@
 
 
 python ngram_acc.py --dataset_name gsm8k \
-    --model_path your_model_path \
-    --model_name llama-3-8B \
+    --model_path deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
+    --model_name deepseek-r1-distill-qwen-1.5b \
     --device "cuda:0" \
     --n 5 \
     --model_type base
